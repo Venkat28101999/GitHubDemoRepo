02b645c1df3efb441bdd33ae3c92bfeb16469138
rajeshwari <rajeshwari@Rajeswari>
2022-02-04 21:07:51 +0530

    first commit

---
+++ SeleniumDemoProject/src/main/java/demo/TestNgDemoClass.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TestNgDemoClass {
+	@Test
+	public static void testCase() {
+		System.out.println("Hello testng i am jenkins i will run you as maven project in my dash board");
+	}
+	
+//	public static void main(String[] args) {
+//		testCase();
+//	}
+}
